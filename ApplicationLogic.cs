4f50f1945c0dc25753e58743278843f9b1765e4c
Sacracia <tiger3551999@mail.ru>
2024-02-25 23:47:25 +0300

    (25.02.2024)

---
+++ ApplicationLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModificaWPF
+{
+    internal class ApplicationLogic
+    {
+        private static ApplicationLogic s_instance;
+
+        public static ApplicationLogic Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                    s_instance = new ApplicationLogic();
+                return s_instance;
+            }
+        }
+
+        public void NavigateTo(Type page)
+        {
+            if (Application.Current.MainWindow is MainWindow window)
+            {
+                window.FrameContent = Application.Current.FindResource(page.Name);
+            }
+        }
+
+        public void NavigateTo<TPage>() where TPage : Page => NavigateTo(typeof(TPage));
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ModificaWPF.Pages;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,24 @@ namespace ModificaWPF
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private object frameContent;
+
+        public object FrameContent
+        {
+            get => frameContent;
+            set
+            {
+                frameContent = value;
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            ApplicationLogic.Instance.NavigateTo<WelcomePage>();
         }
 
         private void CloseClick(Object sender, RoutedEventArgs e)
@@ -33,6 +49,13 @@ namespace ModificaWPF
         private void MinimizeClick(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
